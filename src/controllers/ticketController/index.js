21c72541e46a3acd166dae0a4646b102391b2b54
Pedro Resende <professorpedroresende@gmail.com>
2025-12-04 15:50:43 -0300

    feature/addingPutMethod

--- src/controllers/ticketController/index.js
+++ src/controllers/ticketController/index.js
@@ -1,11 +1,10 @@
 export function index({req,res,database}) { 
     const { status } = req.query ; 
 
+    const filters = status ? { status } : null ;
 
+    const tickets = database.select("tickets", filters); 
+        
+    return res.end(JSON.stringify(tickets)); 
 
-    const tickets = database.select("tickets"); 
-    
-    const statusTickets = tickets.filter((ticket) => ticket.status === status); 
-    
-    return res.end(JSON.stringify(statusTickets))
 }
